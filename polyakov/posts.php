7898bf47ca85f10b73dfc54894b4f806e2294030
Tweaster <f.rasoami@rocketmail.com>
2015-04-06 13:01:13 -0400

    Create posts.php

---
+++ polyakov/posts.php
@@ -0,0 +1,29 @@
+
+
+<article class="intro">
+
+    <div class="<?php echo($post_status); ?>">
+
+    <a class="post-link" href="<?php echo($post_link); ?>" title="<?php echo($post_title); ?>"></a>
+
+    <div class="look" style="background-image: url('<?php echo($post_image); ?>');">
+    </div>
+            
+    <div class="read">
+        <div class="post">
+            <h2><?php echo($post_title); ?></h2>
+            
+            <span class="meta"><?php echo($published_date); ?> by  <?php echo($post_author); ?></span>
+            
+            <p><?php echo($post_intro); ?></p>
+        </div>
+    </div>
+
+    <div class="readmore"></div>
+    
+
+    <div class="shadow"></div> 
+
+    </div>
+
+</article>
